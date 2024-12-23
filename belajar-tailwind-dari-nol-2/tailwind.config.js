14d118b9209a3eebcf60bb9238b08fd339f720f7
ikhsan090502 <alqordowi97@gmail.com>
2024-12-23 20:18:45 +0700

    Initial commit

---
+++ belajar-tailwind-dari-nol-2/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Plus Jakarta Sans", "serif"],
+      custom: ["Courier New", "Courier", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
